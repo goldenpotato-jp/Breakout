45aa838f823a48a5ab1ea7ea3794095a022c1e41
goldenpotato <okina.etsushi@gmail.com>
2025-05-01 09:57:36 +0900

    プレイ画面用関数実装

--- v1.0/main.py
+++ v1.0/main.py
@@ -9,14 +9,19 @@ pyxel.load("/Users/macbook/goldenpotato/python/Breakout/v1.0/my_resource.pyxres"
 
 def update():
 	if(game_state == "TITLE"):update_title_state()
+	elif(game_state == "PLAY"):update_play_state()
 
 def draw():
 	pyxel.cls(0)
 	if(game_state == "TITLE"):draw_title_state()
+	elif(game_state == "PLAY"):draw_play_state()
 
 def update_title_state():
 	global game_state
 	if(pyxel.btn(pyxel.KEY_SPACE)):game_state = "PLAY"
+
+def update_play_state():
+	pass
 
 def draw_title_state():
 	pyxel.cls(1)
@@ -24,5 +29,8 @@ def draw_title_state():
 	pyxel.text(30, 30, "Breakout", 10)
 	pyxel.text(30, 60, "Press [Space] to start", 7)
 	pyxel.text(30, 80, "Press [Esc] to quit", 13)
+
+def draw_play_state():
+	pass
 
 pyxel.run(update, draw)
