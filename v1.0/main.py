508c106407d6144c8a9df471c49e4f9798848967
goldenpotato <okina.etsushi@gmail.com>
2025-05-01 21:09:27 +0900

    テストコミット

---
+++ v1.0/main.py
@@ -0,0 +1,28 @@
+import pyxel
+
+game_state = "TITLE"
+
+#pyxel edit "/Users/macbook/goldenpotato/python/Breakout/v1.0/my_resource.pyxres"
+
+pyxel.init(160, 120, title = "Breakout")
+pyxel.load("/Users/macbook/goldenpotato/python/Breakout/v1.0/my_resource.pyxres")
+
+def update():
+	if(game_state == "TITLE"):update_title_state()
+
+def draw():
+	pyxel.cls(0)
+	if(game_state == "TITLE"):draw_title_state()
+
+def update_title_state():
+	global game_state
+	if(pyxel.btn(pyxel.KEY_SPACE)):game_state = "PLAY"
+
+def draw_title_state():
+	pyxel.cls(1)
+	pyxel.mouse(True)
+	pyxel.text(30, 30, "Breakout", 10)
+	pyxel.text(30, 60, "Press [Space] to start", 7)
+	pyxel.text(30, 80, "Press [Esc] to quit", 13)
+
+pyxel.run(update, draw)
