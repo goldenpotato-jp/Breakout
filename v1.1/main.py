da42181552ab7a4f67c8f3ec6fa60113b1b4a242
goldenpotato <okina.etsushi@gmail.com>
2025-05-01 21:07:49 +0900

    復元

--- v1.1/main.py
+++ v1.1/main.py
@@ -11,19 +11,20 @@ class Paddle:
 		pass
 
 	def draw_paddle(self):
-		pyxel.rect(self.x, self.y, self.width, self.height)
+		pyxel.rect(self.x, self.y, self.width, self.height, 7)
 
 	def set_to_center(self, y):
 		self.y = y
 		self.x = (pyxel.width / 2) - (self.width / 2)
+paddle = Paddle(0, 0, 30, 5)
 
 game_start_frame = 0
 game_state = "TITLE"
 
-#pyxel edit "/Users/macbook/goldenpotato/python/Breakout/v1.0/my_resource.pyxres"
+#pyxel edit "/Users/macbook/goldenpotato/python/Breakout/v1.1/my_resource.pyxres"
 
 pyxel.init(160, 120, title = "Breakout")
-pyxel.load("/Users/macbook/goldenpotato/python/Breakout/v1.0/my_resource.pyxres")
+pyxel.load("/Users/macbook/goldenpotato/python/Breakout/v1.1/my_resource.pyxres")
 
 def update():
 	if(game_state == "TITLE"):update_title_state()
@@ -39,6 +40,7 @@ def update_title_state():
 	if(pyxel.btn(pyxel.KEY_SPACE)):
 		game_state = "PLAY"
 		game_start_frame = pyxel.frame_count
+		paddle.set_to_center(100)
 
 def update_play_state():
 	pass
@@ -54,6 +56,7 @@ def draw_play_state():
 	pyxel.cls(1)
 	if(pyxel.frame_count - game_start_frame < 240):
 		draw_countdown(pyxel.frame_count - game_start_frame)
+	paddle.draw_paddle()
 
 def draw_countdown(t):
 	if(t < 60):pyxel.text(78, 60, "3", 11)
