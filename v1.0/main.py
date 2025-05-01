acf0828633ad17ef8f59a325913eb0287f5dcd7b
goldenpotato <okina.etsushi@gmail.com>
2025-05-01 14:24:13 +0900

    パドルの実装①

--- v1.0/main.py
+++ v1.0/main.py
@@ -1,5 +1,18 @@
 import pyxel
 
+class Paddle:
+	def __init__(self, x, y, width, height):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+
+	def update_paddle():
+		pass
+
+	def draw_paddle():
+		pass
+
 game_start_frame = 0
 game_state = "TITLE"
 
